968a80940ec4e671e42d8731aca5b53958c9b5bd
Abigail Olaguivel <abbyog27@gmail.com>
2024-03-04 18:34:09 -0600

    feat: cleaned up code so far

--- BackEnd/SuperQuiz/super_quiz_app/urls.py
+++ BackEnd/SuperQuiz/super_quiz_app/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('questions/<int:question_id>/', views.question_detail, name="question_detail"),
     path('', views.home, name="home"),
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
@@ -11,7 +10,6 @@ urlpatterns = [
     path('users/<username>/', views.profile, name='profile'),
     path('title/create/', views.create_title, name='create_title'),
     # path('title/edit/<int:quiz_id>/', views.edit_title, name='edit_title'),
-    path('profile/delete/', views.delete_profile, name='delete_profile'),
     path('generate_quiz/', views.generate_quiz, name='generate_quiz'),
     path('start_quiz/', views.start_quiz, name='start_quiz'),
     path('random/', views.random_question, name='random_question'),
--- BackEnd/SuperQuiz/super_quiz_app/views.py
+++ BackEnd/SuperQuiz/super_quiz_app/views.py
@@ -17,6 +17,91 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login as auth_login 
 
+
+
+def login(request):
+    """ login to app"""
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username=u, password=p)
+            if user is not None:
+                if user.is_active:
+                    auth_login(request, user)
+                    return redirect('/', username=u)
+                else:
+                    print(f'{u} - account has been disabled')
+                    return HttpResponseRedirect('/login')
+            else:
+                print('The username and/or password is none')
+                return HttpResponseRedirect('/login')
+        else:
+            print('The username and/or password is not valid')
+            return HttpResponseRedirect('/login')
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', { 'form': form })
+    
+def logout(request):
+    return HttpResponseRedirect('/login')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/login')
+        else:
+            return render(request, 'signup.html', { 'form': form })
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', { 'form': form })
+    
+@login_required
+def profile(request, username):
+    user = User.objects.get(username=username)
+    quiz_title = Quiz.objects.filter(user=user).first()  # Query for the quiz title
+    print(quiz_title)
+    return render(request, 'profile.html', {'user': user, 'quiz_title': quiz_title})
+
+def home(request):
+    return render(request, "home.html")
+
+def create_title(request):
+    if request.method == 'POST':
+        form = QuizForm(request.POST)
+        if form.is_valid():
+            quiz = form.save(commit=False)
+            quiz.user = request.user
+            quiz.pub_date = timezone.now()
+            quiz.save()
+            return redirect('profile', username=request.user.username)  # Redirect to the profile page after creating the quiz
+    else:
+        form = QuizForm()
+    return render(request, 'edit_title.html', {'form': form})
+
+# def edit_title(request, quiz_id):
+#     # Retrieve the quiz object from the database
+#     quiz = get_object_or_404(Quiz, id=quiz_id, user=request.user)
+
+#     if request.method == 'POST':
+#         # Create a form instance and populate it with data from the request
+#         form = EditQuizForm(request.POST, instance=quiz)
+#         if form.is_valid():
+#             # Save the form with the updated quiz title
+#             form.save()
+#             # Redirect to the updated quiz detail page
+#             return redirect('edit_quiz')
+#     else:
+#         # If it's a GET request, create a form instance with the quiz data
+#         form = EditQuizForm(instance=quiz)
+
+    # Render the edit quiz template with the form
+    # return render(request, 'edit_quiz.html', {'form': form,})
+
+
 def generate_quiz(num_questions):
     total_questions = Question.objects.count()
     num_questions = min(num_questions, total_questions)
@@ -76,95 +161,5 @@ def random_question(request):
         'answer_disabled' : False
     })
 
-def home(request):
-    return render(request, "home.html")
 
-def login(request):
-    """ login to app"""
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            u = form.cleaned_data['username']
-            p = form.cleaned_data['password']
-            user = authenticate(username=u, password=p)
-            if user is not None:
-                if user.is_active:
-                    auth_login(request, user)
-                    return redirect('/', username=u)
-                else:
-                    print(f'{u} - account has been disabled')
-                    return HttpResponseRedirect('/login')
-            else:
-                print('The username and/or password is none')
-                return HttpResponseRedirect('/login')
-        else:
-            print('The username and/or password is not valid')
-            return HttpResponseRedirect('/login')
-    else:
-        form = AuthenticationForm()
-        return render(request, 'login.html', { 'form': form })
-    
-def logout(request):
-    return HttpResponseRedirect('/login')
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/login')
-        else:
-            return render(request, 'signup.html', { 'form': form })
-    else:
-        form = UserCreationForm()
-        return render(request, 'signup.html', { 'form': form })
-    
-@login_required
-def profile(request, username):
-    user = User.objects.get(username=username)
-    quiz_title = Quiz.objects.filter(user=user).first()  # Query for the quiz title
-    print(quiz_title)
-    return render(request, 'profile.html', {'user': user, 'quiz_title': quiz_title})
-
-# def quiz_list(request):
-#     title = Quiz.objects.filter(user=request.user)
-#     return render(request, 'profile.html', {'title': title})
-
-def create_title(request):
-    if request.method == 'POST':
-        form = QuizForm(request.POST)
-        if form.is_valid():
-            quiz = form.save(commit=False)
-            quiz.user = request.user
-            quiz.pub_date = timezone.now()
-            quiz.save()
-            return redirect('profile', username=request.user.username)  # Redirect to the profile page after creating the quiz
-    else:
-        form = QuizForm()
-    return render(request, 'edit_title.html', {'form': form})
-
-# def edit_title(request, quiz_id):
-#     # Retrieve the quiz object from the database
-#     quiz = get_object_or_404(Quiz, id=quiz_id, user=request.user)
-
-#     if request.method == 'POST':
-#         # Create a form instance and populate it with data from the request
-#         form = EditQuizForm(request.POST, instance=quiz)
-#         if form.is_valid():
-#             # Save the form with the updated quiz title
-#             form.save()
-#             # Redirect to the updated quiz detail page
-#             return redirect('edit_quiz')
-#     else:
-#         # If it's a GET request, create a form instance with the quiz data
-#         form = EditQuizForm(instance=quiz)
-
-    # Render the edit quiz template with the form
-    # return render(request, 'edit_quiz.html', {'form': form,})
-
-def delete_profile(request):
-    if request.method == 'POST':
-        deleteUser = User.objects.get(username=request.user)
-        deleteUser.delete()
-        return redirect('/signup')
-    return render(request, 'delete_account.html')
